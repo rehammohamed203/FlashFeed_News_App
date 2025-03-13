d16bc143bee7725581dbf5a6dc6405908314dbfe
rehammohammed172003 <94254158+rehammohammed172003@users.noreply.github.com>
2025-03-13 12:52:40 +0200

    feat: Home activity

---
+++ app/src/main/java/com/reham11203/flashfeed/ui/home/HomeActivity.kt
@@ -0,0 +1,72 @@
+package com.reham11203.flashfeed.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.reham11203.flashfeed.R
+import com.reham11203.flashfeed.databinding.ActivityHomeBinding
+import com.reham11203.flashfeed.ui.home.fragments.categories.CategoriesFragment
+import com.reham11203.flashfeed.ui.home.fragments.categories.Category
+import com.reham11203.flashfeed.ui.home.fragments.news.NewsFragment
+
+
+class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.appBarHome.toolBar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
+        showCategoriesFragment(
+            CategoriesFragment.getInstance(
+                onCategoryClickCallback = ::onCategoryClick
+            )
+        )
+        initNavView()
+
+    }
+
+    private fun initNavView() {
+        binding.navView.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    showCategoriesFragment(
+                        CategoriesFragment.getInstance(
+                            onCategoryClickCallback = ::onCategoryClick
+                        )
+                    )
+                }
+            }
+            binding.drawerLayout.close()
+            return@setNavigationItemSelectedListener true
+
+        }
+    }
+
+    private fun onCategoryClick(category: Category) {
+        showNewsFragment(category)
+
+    }
+
+    private fun showCategoriesFragment(fragment: CategoriesFragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.news_fragment_container, fragment)
+            .commit()
+    }
+
+    private fun showNewsFragment(category: Category) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.news_fragment_container, NewsFragment.getInstance(category))
+            .addToBackStack(null)
+            .commit()
+    }
+
+}
